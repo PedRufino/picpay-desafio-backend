6455490f5d951fd0d32d0aa7c73b0d6c6a456a90
Pedro Rufino <pedro.rmn19@gmail.com>
2024-07-13 15:32:55 -0300

    [fix]: domain config

--- src/main/java/com/picpaydesafiobackend/domain/user/User.java
+++ src/main/java/com/picpaydesafiobackend/domain/user/User.java
@@ -1,10 +1,8 @@
 package com.picpaydesafiobackend.domain.user;
 
+import com.picpaydesafiobackend.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -13,13 +11,14 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of="id")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String firstname;
-    private String lastname;
+    private String firstName;
+    private String lastName;
 
     @Column(unique = true)
     private String document;
@@ -30,4 +29,14 @@ public class User {
     private BigDecimal balance;
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data){
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+        this.document = data.document();
+        this.email = data.email();
+    }
 }
