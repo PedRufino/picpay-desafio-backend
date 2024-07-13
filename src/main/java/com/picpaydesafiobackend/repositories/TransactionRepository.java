8a098aa882323e517c3c81a0733ad9bc9e6b26a0
Pedro Rufino <pedro.rmn19@gmail.com>
2024-07-13 11:10:01 -0300

    [Feat]: create TransactionRepository

---
+++ src/main/java/com/picpaydesafiobackend/repositories/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.picpaydesafiobackend.repositories;
+
+import com.picpaydesafiobackend.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
